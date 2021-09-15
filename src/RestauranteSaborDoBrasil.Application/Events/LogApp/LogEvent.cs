0cbb158a76e389f6301834d950cbe99680fbf7a8
Jarbas Baltazar <jarbas_baltazarr@hotmail.com>
2021-09-14 21:22:03 -0300

    Ajusting log versioning

---
+++ src/RestauranteSaborDoBrasil.Application/Events/LogApp/LogEvent.cs
@@ -0,0 +1,13 @@
+﻿using RestauranteSaborDoBrasil.Domain.Core.Messages;
+using RestauranteSaborDoBrasil.Domain.Enums;
+using System;
+
+namespace RestauranteSaborDoBrasil.Application.Events.LogApp
+{
+    public class LogEvent : EventRequest
+    {
+        public TipoLog Tipo { get; set; }
+        public string Mensagem { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+    }
+}
